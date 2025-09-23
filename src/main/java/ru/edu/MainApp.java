37ab28db51b579f01b6bd0fe17193b01d2617143
OlegShikunoff <shikunoff777@yandex.ru>
2025-09-23 18:40:20 +0300

    Добавил каркас приложения

---
+++ src/main/java/ru/edu/MainApp.java
@@ -0,0 +1,14 @@
+package ru.edu;
+
+import ru.edu.app.Application;
+
+/**
+ * Основной класс приложения
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        new Application().run();
+    }
+
+}
